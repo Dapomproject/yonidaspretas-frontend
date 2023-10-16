d01b0a6a21815815ee42579bdd4b88c1007a54da
vandogouveia <vando_gouveia@hotmail.com>
2023-10-16 18:55:19 -0300

    commit

---
+++ src/app/app-routing.modules.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/public/public.module').then(m => m.PublicModule),
+  },
+  {
+    path: 'private',
+    loadChildren: () => import('./modules/private/private.module').then(m => m.PrivateModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    anchorScrolling: 'enabled',
+    scrollPositionRestoration: 'top',
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
