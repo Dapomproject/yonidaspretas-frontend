a50963ec78da02629e4c8d6c2aad7687736ab3d3
vandogouveia <vando_gouveia@hotmail.com>
2025-01-24 10:44:43 -0300

    ajustes perfis

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     API_IBGE: 'https://servicodados.ibge.gov.br/api/v1/localidades/estados',
     API_URL: 'https://api.yonidaspretas.com',
-    //API_URL: 'http://localhost:3001',
+    //API_URL: 'http://localhost:2053',
     USER_TYPE: {
       CLIENTE: 'client',
       ADM: 'adm'
